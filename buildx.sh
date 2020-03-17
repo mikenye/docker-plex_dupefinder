562f99cef22f820a118a9d09cb150b1f1b213124
mikenye <mike.nye@xpanse.com.au>
2020-03-17 21:22:26 +0800

    Fix build script.

--- buildx.sh
+++ buildx.sh
@@ -6,8 +6,8 @@ IMAGE=mikenye/plex_dupefinder
 
 # Build the image using buildx
 docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker pull ${IMAGE}:latest
 
 VERSION=$(docker run --rm --entrypoint cat ${IMAGE}:latest /VERSION | cut -c1-14)
 
 docker buildx build -t ${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
-
